3fa6849cacc2e3a025f63f2c51dc6bd6f4e50f17
srikanthpragada <srikanthpragada@gmail.com>
2022-07-23 19:54:28 +0530

    23-JUL

---
+++ demo/basics/for_demo.py
@@ -0,0 +1,7 @@
+for n in range(1, 11):
+    print(n, end=' ')
+
+print()
+
+for n in range(10, 0, -1):
+    print(n, end=' ')
---
+++ demo/basics/table.py
@@ -0,0 +1,5 @@
+num = int(input("Enter number :"))
+
+for i in range(1, 21):
+    print(f"{num:3} * {i:2} = {num * i:5}")
+
